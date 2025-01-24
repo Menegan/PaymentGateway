e97a867a5e06c6d678044d5bc6c9e000c13ebd80
Oloyede micheal <menegan001@gmail.com>
2025-01-24 20:38:55 +0100

    restructure

--- payment_gateway/payments/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Payment
-
-@admin.register(Payment)
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer_name', 'customer_email', 'amount', 'status', 'reference', 'created_at')
-    list_filter = ('status',)
-    search_fields = ('customer_name', 'customer_email', 'reference')
