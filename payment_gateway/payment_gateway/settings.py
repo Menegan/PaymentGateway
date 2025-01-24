ad49ad7b48e11fbbe7b8d6ee2be46f4e6b50c967
Oloyede micheal <menegan001@gmail.com>
2025-01-24 15:30:00 +0100

    Prepare for Render deployment

--- payment_gateway/payment_gateway/settings.py
+++ payment_gateway/payment_gateway/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for payment_gateway project.
 
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--e-j9np149nhu(63z5k&=*%u!o4zz00nr^ftwdhwg3!o@z&khe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["PaymentGateway.onrender.com"]
 
 
 # Application definition
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
